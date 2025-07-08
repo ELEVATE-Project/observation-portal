36cdd7070557a9df88da8be92a2ed982d63942a5
farhanp1502 <farhanpasha1502@gmail.com>
2025-07-08 16:01:16 +0530

    chnages

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8181;
+const port = process.env.PORT || 9191;
 
 // Serve static files from Angular build directory
 const buildPath = path.join(__dirname, 'dist', 'observation-portal', 'browser');
