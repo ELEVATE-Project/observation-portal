c58de21badafa9f5e04e03da213aaafcd1e1bc44
naveen <chennamsettynaveen945@gmail.com>
2025-08-20 13:51:58 +0530

    remove unwanted code base

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -1,6 +1,4 @@
 window["env"] = {
     production: true,
-    // surveyBaseURL: 'https://saas-qa.tekdinext.com',
-    // surveyBaseURL:"https://qa.elevate-apis.shikshalokam.org"
-    surveyBaseURL:"https://dev.elevate-apis.shikshalokam.org"
+    surveyBaseURL: '<surveyBaseURL>'
 }
