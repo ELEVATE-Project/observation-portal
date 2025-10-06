f296535343dba2a317681670f0769bf3ce6449eb
naveen <chennamsettynaveen945@gmail.com>
2025-10-06 19:10:14 +0530

    Survey Reports filters issue

--- src/app/survey-reports/survey-reports.component.ts
+++ src/app/survey-reports/survey-reports.component.ts
@@ -64,7 +64,10 @@ export class SurveyReportsComponent implements OnInit {
   
   }
   surveyReportPdf(type:any){
-    this.apiService.post(urlConfig.survey.reports+`${this.submissionId}&pdf=true`,{})
+    let payload:any={
+      filter:{questionId:this.reportDetails.map(element => element.order)}
+    }
+    this.apiService.post(urlConfig.survey.reports+`${this.submissionId}&pdf=true`,payload)
     .subscribe(async (res:any) => { 
       if(type === 'download'){
         await this.openUrl(res?.message?.pdfLink);
