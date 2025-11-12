49a99126ff8f99949fb9405c6eb8e4f886c40d74
naveen <chennamsettynaveen945@gmail.com>
2025-11-12 15:49:51 +0530

    head title section changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 import { Location } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,8 +10,24 @@ import { Location } from '@angular/common';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  constructor(private location: Location) {}
+  constructor(private location: Location,private router: Router, private activatedRoute: ActivatedRoute,private titleService: Title) {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          let route = this.activatedRoute;
+          while (route.firstChild) route = route.firstChild;
+          return route;
+        }),
+        mergeMap(route => route.data)
+      )
+      .subscribe(data => {
+        if (data['title']) {
+          this.titleService.setTitle(data['title']);
+        }
+      });
+  }
   goBack(): void {
-    this.location.back(); 
+    window.history.go(-1);
   }
 }
