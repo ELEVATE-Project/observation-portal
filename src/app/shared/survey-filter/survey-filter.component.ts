2e84983aec31901495782138fbc135fca66aa501
Syed Ameen <syedameenabrar786@gmail.com>
2025-12-15 15:46:19 +0530

    4296 , 4300 - evidences report issues in survey and observation after filter,

--- src/app/shared/survey-filter/survey-filter.component.ts
+++ src/app/shared/survey-filter/survey-filter.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-survey-filter',
@@ -11,13 +12,18 @@ import { Inject } from '@angular/core';
 export class SurveyFilterComponent {
   allQuestions:any;
   constructor(public dialogRef: MatDialogRef<SurveyFilterComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public toaster: ToastService,
 ){
   this.allQuestions = data.allQuestions;
 }
   applyFilter(){
     const selectedQuestions = this.allQuestions.filter((q: any) => q.selected);
+    if (selectedQuestions.length === 0) {
+      this.toaster.showToast('SELECT_ATLEAST_ONE_QUESTION', 'danger');
+    }else{
     this.dialogRef.close(selectedQuestions);
+    }
   }
   resetFilter(){
     this.allQuestions.forEach((question: any) => {
